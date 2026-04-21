8194489e208ee47989eae0ef045bba820013e48e
Zhuowei95 <zhuowei.wu@sa.stud.vu.lt>
2026-04-22 00:12:42 +0300

    Task 1: created manual JSON file and basic JSON reader

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "users.json";
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found: " + filePath);
+            return;
+        }
+
+        string jsonData = File.ReadAllText(filePath);
+
+User? user = JsonConvert.DeserializeObject<User>(jsonData);
+
+if (user != null)
+{
+    Console.WriteLine("Task 1: Read JSON file");
+    Console.WriteLine("Name: " + user.Name);
+    Console.WriteLine("Age: " + user.Age);
+    Console.WriteLine("City: " + user.City);
+}
+else
+{
+    Console.WriteLine("Failed to deserialize JSON data.");
+}
+    }
+}
